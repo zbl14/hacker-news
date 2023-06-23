b6b6b3de9d01682023397b860fb7745dbdd917f8
Zhibin Liang <ifthereisoneday@gmail.com>
2023-06-23 10:29:02 -0400

    Display current locate date

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { format } from "date-fns";
 
 const Header = () => {
+  const currentDate = format(new Date(), "EEEE, MMMM dd, yyyy");
   return (
     <div>
       <h1>Hacker News Latest Stories</h1>
+      <p>{currentDate}</p>
     </div>
   );
 };
