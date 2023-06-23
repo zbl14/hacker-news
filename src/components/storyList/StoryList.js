3ea3b6916edb8888b30fac63602bd1908610f8f1
Zhibin Liang <ifthereisoneday@gmail.com>
2023-06-23 11:02:21 -0400

    Style the storyList

--- src/components/storyList/StoryList.js
+++ src/components/storyList/StoryList.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import useNewStories from "../../hooks/useNewStories";
 import Story from "./story/Story";
 import Pagination from "./pagination/Pagination";
+import styles from "./StoryList.module.css";
 
 const StoryList = () => {
   useNewStories();
@@ -13,15 +14,15 @@ const StoryList = () => {
   const selectedStories = newStories.slice(start, start + itemsPerPage);
 
   return (
-    <div>
-      <div>
+    <div className={styles.storyListContainer}>
+      <div className={styles.grid}>
         {selectedStories.map((id) => (
-          <div key={id}>
+          <div className={styles.storyContainer} key={id}>
             <Story id={id} />
           </div>
         ))}
       </div>
-      <div>
+      <div className={styles.paginationContainer}>
         <Pagination />
       </div>
     </div>
