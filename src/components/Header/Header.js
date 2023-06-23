61b8dba7886a25fb9ed7782b2025267d95e05939
Zhibin Liang <ifthereisoneday@gmail.com>
2023-06-23 10:42:50 -0400

    Style the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { format } from "date-fns";
+import styles from "./Header.module.css";
 
 const Header = () => {
   const currentDate = format(new Date(), "EEEE, MMMM dd, yyyy");
   return (
-    <div>
-      <h1>Hacker News Latest Stories</h1>
-      <p>{currentDate}</p>
+    <div className={styles.headerContainer}>
+      <h1 className={styles.title}>Hacker News Latest Stories</h1>
+      <p className={styles.date}>{currentDate}</p>
     </div>
   );
 };
