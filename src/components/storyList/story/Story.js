112194fffdeefaa8bfac14edd6b6f5127a918213
Zhibin Liang <ifthereisoneday@gmail.com>
2023-06-23 12:45:36 -0400

    implement icons

--- src/components/storyList/story/Story.js
+++ src/components/storyList/story/Story.js
@@ -3,6 +3,10 @@ import useStory from "../../../hooks/useStory";
 import { formatDistanceToNow } from "date-fns";
 import PropTypes from "prop-types";
 import styles from "./Story.module.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 
 const Story = ({ id }) => {
   const { story, isLoading } = useStory(id);
@@ -26,12 +30,30 @@ const Story = ({ id }) => {
             {title}
           </a>
         </h2>
-        <p className={styles.cardBody}>By {by}</p>
+        {/* <p className={styles.cardBody}>By {by}</p>
         <p className={styles.cardBody}>
           Posted: {formatDistanceToNow(date)} ago
         </p>
         <p className={styles.cardBody}>{descendants} Comments</p>
-        <p className={styles.cardBody}>{score} Likes</p>
+        <p className={styles.cardBody}>{score} Likes</p> */}
+        <p className={styles.cardBody}>
+          <AccountCircleIcon className={styles.iconStyle} />
+          <span className={styles.textStyle}>{by}</span>
+        </p>
+        <p className={styles.cardBody}>
+          <AccessTimeIcon className={styles.iconStyle} />
+          <span className={styles.textStyle}>
+            {formatDistanceToNow(date)} ago
+          </span>
+        </p>
+        <p className={styles.cardBody}>
+          <ChatBubbleOutlineIcon className={styles.iconStyle} />
+          <span className={styles.textStyle}>{descendants}</span>
+        </p>
+        <p className={styles.cardBody}>
+          <ThumbUpAltIcon className={styles.iconStyle} />
+          <span className={styles.textStyle}>{score}</span>
+        </p>
       </div>
     </div>
   );
