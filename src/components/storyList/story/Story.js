5bd2f9853e563304b960fa12aa8c18e3a4903ddd
Zhibin Liang <ifthereisoneday@gmail.com>
2023-06-23 11:19:39 -0400

    Implement card style

--- src/components/storyList/story/Story.js
+++ src/components/storyList/story/Story.js
@@ -2,6 +2,7 @@ import React from "react";
 import useStory from "../../../hooks/useStory";
 import { formatDistanceToNow } from "date-fns";
 import PropTypes from "prop-types";
+import styles from "./Story.module.css";
 
 const Story = ({ id }) => {
   const { story, isLoading } = useStory(id);
@@ -18,16 +19,20 @@ const Story = ({ id }) => {
   const date = new Date(time * 1000);
 
   return (
-    <div>
-      <h2>
-        <a href={url} target="_blank" rel="noreferrer">
-          {title}
-        </a>
-      </h2>
-      <p>By {by}</p>
-      <p>{descendants} Comments</p>
-      <p>Likes: {score}</p>
-      <p>Posted: {formatDistanceToNow(date)} ago</p>
+    <div className={styles.cardContainer}>
+      <div className={styles.cardContent}>
+        <h2 className={styles.cardTitle}>
+          <a href={url} target="_blank" rel="noreferrer">
+            {title}
+          </a>
+        </h2>
+        <p className={styles.cardBody}>By {by}</p>
+        <p className={styles.cardBody}>
+          Posted: {formatDistanceToNow(date)} ago
+        </p>
+        <p className={styles.cardBody}>{descendants} Comments</p>
+        <p className={styles.cardBody}>{score} Likes</p>
+      </div>
     </div>
   );
 };
