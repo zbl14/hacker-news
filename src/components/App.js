49d077511500d1a6aa8567ea4289d7a17ac7228c
Zhibin Liang <ifthereisoneday@gmail.com>
2023-06-22 22:41:39 -0400

    Reorganized files and created a header

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Hacker News Latest Stories</h1>
+    </div>
+  );
+};
+
+export default Header;
